0963f3fea02e5a96a2804f152a16f671cdaa005a
Gledson Miranda <gmiranda.developer@gmail.com>
2026-02-03 02:37:48 +0000

    feat: use `ServiceError` in `database.js`

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,5 @@
 import { Client } from "pg";
+import { ServiceError } from "./errors.js";
 
 async function query(queryObject) {
   let client;
@@ -8,9 +9,12 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.log("\n Erro do catch do database.js");
-    console.error(error);
-    throw error;
+    const sericeErrorObject = new ServiceError({
+      message: "Erro na conexão com Banco ou na Query.",
+      cause: error,
+    });
+
+    throw sericeErrorObject;
   } finally {
     await client?.end();
   }
